9e7c626cb41b5c45f988ed880a370e06da0eb192
wangxinji111 <18515121213@163.com>
2020-01-11 23:35:51 +0800

    分支提交

--- spring-boot/src/main/java/com/wxj/config/App.java
+++ spring-boot/src/main/java/com/wxj/config/App.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
  */
 /*@ComponentScan({"com.wxj.controller","com.wxj.service"})
 @EnableAutoConfiguration*/
+//自定义数据源，需要添加DataSourceAutoConfiguration否则冲突
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class App {
     //先用扫包的启动方式
